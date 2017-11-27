d029d06c11436519ed6ea698507b6e6de2a150ef
Savaş Cilve <savascilve@outlook.com>
2017-11-27 09:59:01 +0000

    Add project files.

---
+++ BankingKataMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankingKataMVC.Startup))]
+namespace BankingKataMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
